208decfe07439b0b6e35ae52f6b7862ae1d31534
takayuki-ochiai <go.ahead.oti@gmail.com>
2016-12-11 23:52:45 +0900

    [fix] アクセストークンの値を正常に保存できていなかった問題を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,20 @@ class User < ApplicationRecord
   has_many :books, through: :notifiers
 
   # validates :name, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
+  # validates :email, presence: true
+  # validates :encrypted_password, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :validatable,
+         :recoverable, :trackable,
          :omniauthable, omniauth_providers: [:facebook]
 
   after_create :update_access_token!
 
   def update_access_token!
     self.access_token = "#{self.id}:#{Devise.friendly_token}"
-    self.save
+    self.save!
   end
 
   def self.find_by_authentication_token(access_token)
