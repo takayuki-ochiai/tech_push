56419ec714b9204a895df5453876e8a9387f6186
takayuki-ochiai <go.ahead.oti@gmail.com>
2017-01-01 14:58:00 +0900

    [add] push通知送信用のClientクラスを作成

---
+++ app/models/clients/one_signal_api_client.rb
@@ -0,0 +1,31 @@
+class OneSignalApiClient
+  URI = "https://onesignal.com/api/v1"
+  attr_reader :app_id, :rest_api_key
+
+  # 初期化処理
+  # app_idとapi_keyは環境変数から取得を想定
+  def initialize(app_id, rest_api_key)
+    @rest_api_key = rest_api_key || ENV["APP_ID"]
+    @app_id = app_id || ENV["REST_API_KEY"]
+    @conn = Faraday::Connection.new(:url => URI) do |builder|
+      builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
+    end
+  end
+
+  def notifi_messages(headings, contents, target_player_ids)
+    response = @conn.post('/notifications') do |req|
+      req.headers["Content-Type"] = 'application/json'
+      req.headers["Authorization"] = "Basic #{rest_api_key}"
+      req.body = {
+        app_id: app_id,
+        rest_api_key: rest_api_key,
+        headings: headings,
+        contents: contents,
+        include_player_ids: target_player_ids
+      }.to_json
+    end
+
+    apps = JSON.parse(response.body)
+    apps
+  end
+end
