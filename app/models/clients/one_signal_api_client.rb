916aca5d57f916afff5c6af5223807ef802562d1
takayuki-ochiai <go.ahead.oti@gmail.com>
2017-01-07 23:01:14 +0900

    [clean] OneSignalAPIクライアントのリファクタリング

--- app/models/clients/one_signal_api_client.rb
+++ app/models/clients/one_signal_api_client.rb
@@ -1,33 +1,28 @@
-class OneSignalApiClient
-  URI = "https://onesignal.com/api/v1"
-  attr_reader :app_id, :rest_api_key
+module OneSignalApiClient
+  # OneSignalApiClient.notifi_messages(
+  #   {en: "notification test", ja: "通知テストですよ"},
+  #   {en: "This is notification test", ja: "通知のテスト"},
+  #   [user_id]
+  # )
 
-  # 初期化処理
-  # app_idとapi_keyは環境変数から取得を想定
-  def initialize(app_id, rest_api_key)
-    @app_id = app_id || ENV["ONESIGNAL_APP_ID"]
-    @rest_api_key = rest_api_key || ENV["ONESIGNAL_REST_API_KEY"]
-    @conn = Faraday::Connection.new(:url => URI) do |builder|
-      builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
-    end
+  URI = "https://onesignal.com/api/v1"
+  APP_ID = ENV["ONESIGNAL_APP_ID"]
+  REST_API_KEY = ENV["ONESIGNAL_REST_API_KEY"]
+  CONN = Faraday::Connection.new(:url => URI) do |builder|
+    builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
   end
 
-  def notifi_messages(headings, contents, target_player_ids)
-    response = @conn.post('notifications') do |req|
+  def self.notifi_messages(headings, contents, target_player_ids)
+    response = CONN.post('notifications') do |req|
       req.headers["Content-Type"] = 'application/json'
-      req.headers["Authorization"] = "Basic #{rest_api_key}"
+      req.headers["Authorization"] = "Basic #{REST_API_KEY}"
       req.body = {
-        app_id: app_id,
-        # rest_api_key: rest_api_key,
-        # headings: {en: "notification test", ja: "通知テストですよ"},
-        # contents: {en: "This is notification test", ja: "通知のテストですよ"},
-        # included_segments: ["All"]
+        app_id: APP_ID,
         headings: headings,
         contents: contents,
         include_player_ids: target_player_ids
       }.to_json
     end
-    # binding.pry
 
     apps = JSON.parse(response.body)
     apps
