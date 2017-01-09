9712be41213e56e92534dfd0b57e3278aa16a6d5
takayuki-ochiai <go.ahead.oti@gmail.com>
2017-01-09 20:39:10 +0900

    [update] 要件上対象のTopicTreePathのレコードが直属の親子関係を表しているかどうかを表す必要が出てきたため、parental_flgを追加

--- db/migrate/20161030064749_create_topic_tree_paths.rb
+++ db/migrate/20161030064749_create_topic_tree_paths.rb
@@ -3,6 +3,7 @@ class CreateTopicTreePaths < ActiveRecord::Migration[5.0]
     create_table :topic_tree_paths do |t|
       t.integer :ancestor_id, null: false
       t.integer :descendant_id, null: false
+      t.boolean :parental_flg, null: false, default: false
       t.timestamps
     end
 
