850380f489f114c64f8a85a0e45956eb4b71fa52
takayuki-ochiai <go.ahead.oti@gmail.com>
2017-01-15 12:31:45 +0900

    [update] 書籍管理画面の修正 ・topicsの表示順序を修正 ・書籍一覧の表示順位を修正 ・書籍一覧の表示内容を修正 ・表示非表示を制御できるように修正

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -2,36 +2,43 @@ class Admin::BooksController < ApplicationController
   before_action :admin_user!
 
   def index
-    @books = Book.all
+    @books = Book.all.includes(:topics).order("sales_date DESC")
   end
 
   def edit
     @book = Book.find(params[:id])
-    @select_topic_collections = SecondTopic.all
+    @select_topic_collections = SecondTopic.all.order(:name)
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(
+      display_flg: book_params[:display_flg]
+    )
     book_topics_attributes = book_params[:book_topics_attributes]
+
     BookTopic.where(book_id: params[:id]).delete_all
-    book_topics_attributes.values.each { |book_topics_attribute|
-      unless book_topics_attribute[:_destroy] == '1'
-        BookTopic.create(
-          book_id: book_topics_attribute[:book_id],
-          topic_id: book_topics_attribute[:topic_id]
-        )
-      end
-    }
+    if book_topics_attributes.present?
+      book_topics_attributes.values.each { |book_topics_attribute|
+        unless book_topics_attribute[:_destroy] == '1'
+          BookTopic.create(
+            book_id: book_topics_attribute[:book_id],
+            topic_id: book_topics_attribute[:topic_id]
+          )
+        end
+      }
+    end
+
     redirect_to admin_books_path
   end
 
   def destroy
     Book.find(params[:id]).destroy
-    # BookTopic.where(book_id: params[:id]).delete_all
     redirect_to admin_books_path
   end
 
   private
     def book_params
-      params.require(:book).permit(book_topics_attributes: [:id, :book_id, :topic_id, :_destroy])
+      params.require(:book).permit(:display_flg, book_topics_attributes: [:id, :book_id, :topic_id, :_destroy])
     end
 end
