48e77694fffeee909fd8358f98cdeab336fb0278
takayuki-ochiai <go.ahead.oti@gmail.com>
2017-01-29 19:54:48 +0900

    [fix] noticeのvalidationの誤りを修正

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -3,6 +3,6 @@ class Notice < ApplicationRecord
   belongs_to :user
   belongs_to :device
 
-  validates :notified, presence: true
+  validates :notified, inclusion: { in: [true, false] }
   validates :book_id, uniqueness: { scope: :user_id }
 end
