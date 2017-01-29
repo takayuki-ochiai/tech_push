4353140132c967f8cb3a728007d9920def0a953a
takayuki-ochiai <go.ahead.oti@gmail.com>
2017-01-29 19:59:32 +0900

    [clean] 楽天の書籍情報取り込みバッチをリファクタリング

--- app/models/batches/rakuten_books_import_batch.rb
+++ app/models/batches/rakuten_books_import_batch.rb
@@ -1,31 +1,31 @@
 module RakutenBooksImportBatch
-  def self.execute(params = nil, limit_date = Date.today)
-    params = self.create_params(params)
+  def self.execute(
+    params = nil,
+    limit_date = Date.today
+  )
+    params = create_params(params)
 
     while true
       book_items = RakutenBooksApiClient.search(params)
       books = RakutenBooksFormatter.format_items(book_items)
       import_books = books.select { |book| book.sales_date >= limit_date }
       break if import_books.empty?
-      import_books.each do |import_book|
-        import_book.save_with_isbn
-      end
-
+      import_books.each(&:save_with_isbn)
       params[:page] += 1
     end
   end
 
   private
-    def self.create_params(params)
-      default_params = {
-        sort: "-releaseDate",
-        booksGenreId: '001005',
-        page: 1
-      }
+  def self.create_params(params)
+    default_params = {
+      sort: '-releaseDate',
+      booksGenreId: '001005',
+      page: 1
+    }
 
-      return default_params unless params.present?
+    return default_params unless params.present?
 
-      params.merge(default_params)
-      params
-    end
+    params.merge(default_params)
+    params
+  end
 end
