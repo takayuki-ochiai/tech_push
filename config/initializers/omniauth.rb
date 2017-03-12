fd69f8292869db86ed3e89b68562853a4d091137
takayuki-ochiai <go.ahead.oti@gmail.com>
2017-03-12 22:12:51 +0900

    [fix] nginx導入によってfacebookログイン時のリダイレクトurlが正常でなくなってしまった問題の修正 ・環境変数だと読み込みうまくいかないみたいなので、一回ベタ張りする。urlだけなのでセキュリティ的にも一応問題なし

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: "#{ENV['SERVER_NAME']}/users/auth/facebook/callback"
+  server_name = 'https://www.tech-push.com' if Rails.env.production?
+  server_name = 'https://52.199.74.173' if Rails.env.staging?
+  server_name = 'https://local.examples.com.dev:9292' unless server_name.present?
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: "#{server_name}/users/auth/facebook/callback"
 end
